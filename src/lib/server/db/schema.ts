49b43c59b210af42e0864f87d67cf2b2be3ede62
Lucas Garbe <hi@lucasgarbe.de>
2025-12-07 17:10:24 +0100

    feat: use postgresql

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,18 +1,17 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
-export const Events = sqliteTable("events", {
-	id: integer("id").primaryKey().unique().notNull(),
-	title: text("title").notNull().default(""),
-	description: text("description").notNull().default(""),
-	start: text("start_time").notNull(),
-	end: text("end_time").notNull(),
-	club: integer("club").references(() => Clubs.id),
-	createdAt: integer("created_at").notNull(),
+export const Events = pgTable('events', {
+	id: serial('id').primaryKey().unique().notNull(),
+	title: text('title').notNull().default(''),
+	description: text('description').notNull().default(''),
+	start: text('start_time').notNull(),
+	end: text('end_time').notNull(),
+	club: integer('club').references(() => Clubs.id),
+	createdAt: timestamp('created_at').notNull().defaultNow()
 });
 
-export const Clubs = sqliteTable("clubs", {
-	id: integer("id").primaryKey().unique().notNull(),
-	name: text("name").notNull().default(""),
-	color: text("color"),
+export const Clubs = pgTable('clubs', {
+	id: serial('id').primaryKey().unique().notNull(),
+	name: text('name').notNull().default(''),
+	color: text('color')
 });
-
