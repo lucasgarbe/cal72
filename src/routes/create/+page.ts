3f914a68e5f0ba71503a1014223fc134d640e8f5
Lucas Garbe <hi@lucasgarbe.de>
2025-12-07 18:11:11 +0100

    fix: missing clubs in create event form

--- src/routes/create/+page.ts
+++ src/routes/create/+page.ts
@@ -1,7 +1,8 @@
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ url }) => {
+export const load: PageLoad = async ({ url, data }) => {
 	return {
+		...data,
 		date: url.searchParams.get('date') || null
 	};
-}
+};
