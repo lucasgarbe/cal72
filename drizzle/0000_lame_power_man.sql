d08d66479ed73cf95871e510da3ceda7984627cb
Lucas Garbe <hi@lucasgarbe.de>
2025-12-16 09:54:42 +0100

    fix: update sql migration

--- drizzle/0000_lame_power_man.sql
+++ drizzle/0000_lame_power_man.sql
@@ -1,10 +1,10 @@
-CREATE TABLE "clubs" (
+CREATE TABLE IF NOT EXISTS "clubs" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"name" text DEFAULT '' NOT NULL,
 	"color" text
 );
 --> statement-breakpoint
-CREATE TABLE "events" (
+CREATE TABLE IF NOT EXISTS "events" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"title" text DEFAULT '' NOT NULL,
 	"description" text DEFAULT '' NOT NULL,
@@ -14,4 +14,8 @@ CREATE TABLE "events" (
 	"created_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
-ALTER TABLE "events" ADD CONSTRAINT "events_club_clubs_id_fk" FOREIGN KEY ("club") REFERENCES "public"."clubs"("id") ON DELETE no action ON UPDATE no action;
+DO $$ BEGIN
+ ALTER TABLE "events" ADD CONSTRAINT "events_club_clubs_id_fk" FOREIGN KEY ("club") REFERENCES "public"."clubs"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
